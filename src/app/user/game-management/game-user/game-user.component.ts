1d91c7b7283f128388f7a8c333f6dbe0f4ec1d6e
cubo142 <doducminh8694@gmail.com>
2022-06-20 23:24:10 +0700

    trang user

---
+++ src/app/user/game-management/game-user/game-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-user',
+  templateUrl: './game-user.component.html',
+  styleUrls: ['./game-user.component.css']
+})
+export class GameUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
